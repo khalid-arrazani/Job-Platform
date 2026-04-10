f04747c0c7d8c78aa25b012d3ec8d111c8fe3f12
khalid192 <karazani2003@gmail.com>
2026-04-10 23:01:12 +0100

    .

--- models/User.js
+++ models/User.js
@@ -1,35 +1,44 @@
 import mongoose from "mongoose";
 import Joi from "joi";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true });
+      type: Boolean,
+      default: false,
+    },
+    resetCode: String,
 
+    resetCodeExpire: Date,
+  },
+  { timestamps: true },
+);
+userSchema.methods.generateToken = function( ){
+  return jwt.sign({id: this._id, isAdmin: this.isAdmin},process.env.JWT_SECRET_KEY,{ expiresIn: "7d" })
+};
 
 export const validateUserRegistration = (user) => {
-    const schema = Joi.object({
-        username: Joi.string().min(3).max(30).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required()
-    });
-    return schema.validate(user);
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(124).required(),
+  });
+  return schema.validate(user);
 };
 
 export default mongoose.model("User", userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -54,6 +54,11 @@ router.post('/login',asyncHandler(async (req, res) => {
         return res.status(404).json({ message: 'User not found' });
     }
 
+    //TOKEN GENERATION LOGIC WOULD GO HERE
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
 
     res.json({ message: 'Login successful!' });
 }));
