af46c4f903b96cf109a2fbc3ae10949b8ca7df6a
khalid <karazani2003@gmail.com>
2026-07-10 20:55:37 +0100

    vv

--- models/Company.js
+++ models/Company.js
@@ -132,7 +132,7 @@ export const companyValidation = (data) => {
   return Joi.object({
     name: Joi.string().trim().min(2).max(100).required(),
 
-    description: Joi.string().trim().min(20).max(2000),
+    description: Joi.string().trim().min(20).max(2000).allow(""),
 
     company_number: Joi.string().trim().min(4).max(20).required(),
 
