7f4a68f531036e1b16cc33c629b37a9234c2fc82
khalid <karazani2003@gmail.com>
2026-08-06 18:34:25 +0100

    bbb

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,14 +114,14 @@ export const loginUser = asyncHandler(async (req, res) => {
 
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
    sameSite: "none",
     maxAge: 15 * 60 * 1000 // 15 minutes
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
