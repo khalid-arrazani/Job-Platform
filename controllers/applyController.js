02294ea5b153c4d5ca01e341868ec8c86b534fac
khalid <karazani2003@gmail.com>
2026-08-25 19:32:38 +0100

    1111

--- controllers/applyController.js
+++ controllers/applyController.js
@@ -134,6 +134,7 @@ export const getJobApplications = asyncHandler(async (req, res) => {
   }
   const filterFields = ["status"]
 
+  
   filterFields.forEach((field) => {
     if (req.query[field] !== undefined &&
       req.query[field].length >= 1 &&
@@ -146,7 +147,7 @@ export const getJobApplications = asyncHandler(async (req, res) => {
   const search = req.query.search || ""
   const sort = req.query.sort == "Newest First" ? -1 : req.query.sort == "Oldest First" ? 1 : -1
 
-  
+
   const applications = await Apply.find(
     filter,
     "status createdAt"
